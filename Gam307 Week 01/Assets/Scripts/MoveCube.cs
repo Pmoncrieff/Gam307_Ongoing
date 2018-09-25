4a7a6e2f9ae979244a61dcba0e4ff59fe7bdbf07
Patrick Moncrieff <Pmoncrieff@myjmc.edu.au>
2018-09-25 12:02:30 +1000

    MoveCube

---
+++ Gam307 Week 01/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+
+    int rnd = Random.Range(0, 360);
+            
+        transform.Rotate(new Vector3(0, rnd, 0));
+
+        //transform.Rotate(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+            
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.E))
+            transform.Translate(new Vector3(0, 0, Time.deltaTime * speed));
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+	    
+}
